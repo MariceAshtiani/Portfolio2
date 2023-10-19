0a5a091b2636781741f633fa49373f199b09ade2
MariceAshtiani <marice95@hotmail.no>
2023-10-19 18:42:27 +0200

    installed dependencies, removed unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+//import Theme from "./Styles/Theme.jsx";
+//import GlobalStyle from "./Styles/GlobalStyles.jsx";
+//import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
