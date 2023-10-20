3a1958a12d91f5d1d961f703d9d1291381dd5e65
MariceAshtiani <marice95@hotmail.no>
2023-10-20 22:45:57 +0200

    fixed footer-size on mobile

--- src/Components/Layout/Footer/styled.jsx
+++ src/Components/Layout/Footer/styled.jsx
@@ -1,18 +1,26 @@
 import styled from "styled-components";
 
 export default styled.footer`
-            display: flex;
-            justify-content: center;
-            width: 100%;
-            padding: 20px 0px;
-            margin: 0 auto;
-            background-color: #1c0924;
-            background-image: linear-gradient(43deg, #1c0924 60%, #2e0e3b 100%);
-            border: 5px double rgb(209, 167, 181);
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding: 20px 0px;
+    margin: 0 auto;
+    background-color: #1c0924;
+    background-image: linear-gradient(43deg, #1c0924 60%, #2e0e3b 100%);
+    border: 5px double rgb(209, 167, 181);
             
 
-            p {
-                margin: 0;
-                color: rgb(209,167,181)
-            }
-            `;
+    p {
+        margin: 0;
+        color: rgb(209,167,181)
+    }
+
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+        p {
+            font-size: 0.8rem;
+        }
+    }
+
+    
+`;
