ad5f94bb5607faa60ce637f3fcbc0a3834427cc1
MariceAshtiani <marice95@hotmail.no>
2023-10-20 22:50:02 +0200

    empty card-component

---
+++ src/Components/UI/Cards/Projects/index.jsx
@@ -0,0 +1,10 @@
+import StyledCard from "./styled";
+import BasicButton from "../../Buttons/styled";
+
+export default function ProjectCard() {
+    return (
+        <StyledCard>
+            
+        </StyledCard>
+    )
+}
