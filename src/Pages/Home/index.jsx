142ad237a694edeb4c4c4ad2ee4504b772dd4824
MariceAshtiani <marice95@hotmail.no>
2023-10-20 22:47:03 +0200

    added home-page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import { HelmetProvider, Helmet } from "react-helmet-async"
 import StyledHomepage from "./styled";
+import BasicButton from "../../Components/UI/Buttons/styled";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return(
@@ -14,7 +16,24 @@ export default function HomePage() {
                 </Helmet>
             </HelmetProvider>
             <StyledHomepage>
-            <div>HOMEPAGE</div>
+                <main>
+                    <div className="home-info">
+                        <h1>Marice Ashtiani</h1>
+                        <h2>Front End Developer</h2>
+                        <h3>Learn more</h3>
+                        <div className="more-btns">
+                            <Link to="/projects">
+                                <BasicButton>Projects</BasicButton>
+                            </Link>
+                            <Link to="/about">
+                                <BasicButton>About</BasicButton>
+                            </Link>
+                            <Link to="/contact">
+                                <BasicButton>Contact</BasicButton>
+                            </Link>
+                        </div>
+                    </div>
+                </main>
             </StyledHomepage>
             </>
     )
--- src/Pages/Home/styled.jsx
+++ src/Pages/Home/styled.jsx
@@ -1,5 +1,77 @@
 import styled from "styled-components";
 
 export default styled.div`
-    background-color: rgb(28, 9, 63);
+    .home-info {
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        align-items: center;
+        font-family: "vendetta", serif;
+        font-style: normal;
+
+        h1 {
+            font-size: 6rem;
+        }
+
+        h2 {
+            font-size: 3rem;
+            margin-bottom: 10%;
+        }
+
+        h3 {
+            font-size: 2rem;
+        }
+
+        .more-btns {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            gap: 1rem;
+        }
+    }
+    @media (max-width: ${({ theme }) => theme.tablet}) {
+
+        .home-info {
+
+            h1 {
+                font-size: 4rem;
+            }
+
+            h2 {
+                font-size: 2rem;
+                margin-bottom: 10%;
+            }
+
+            h3 {
+                font-size: 1.5rem;
+            }
+        }
+    }
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+        .home-info {
+
+            h1 {
+                font-size: 2rem;
+            }
+
+            h2 {
+                font-size: 1.5rem;
+                margin-bottom: 10%;
+            }
+
+            h3 {
+                font-size: 1rem;
+            }
+
+            .more-btns {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 1rem;
+        }
+        }
+    }
+
 `
